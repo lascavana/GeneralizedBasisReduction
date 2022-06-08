8d2e7176357b181902322e4dd29739c9275c4544
Lara Scavuzzo Montaña <lascavuzzo@gmail.com>
2022-06-08 12:19:40 +0200

    change to scip

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,105 @@
-#include <limits>
 #include <vector>
 #include <sstream>
 #include <iostream>
 
-
-#include "gurobi_c++.h"
+#include <scip/scip.h>
+#include <scip/scipdefplugins.h>
 
 
 using namespace std;
 using matrix = vector<vector<double>>;
 
-string itos(int i) {stringstream s; s << i; return s.str(); }
+void CATCH_SCIP(SCIP_RETCODE retcode)
+{
+  if (retcode!=SCIP_OKAY)
+  {
+    throw "SCIP Error: recode ";
+  }
+}
 
 class Polytope
 {
-  GRBEnv env = GRBEnv();
-  GRBModel model = GRBModel(env);
-
-  vector<GRBVar> xvars;
-  vector<GRBVar> yvars;
+  SCIP* scip;
+  vector<SCIP_VAR*> xvars;
+  vector<SCIP_VAR*> yvars;
+  vector<SCIP_CONS*> conss;
 
   void initialize(const matrix &A, const vector<double> &b,
                   const vector<double> &lb, const vector<double> &ub)
   {
+    SCIP_RETCODE retcode;
     int m = b.size();
     int n = lb.size();
-    assert(lb.size()==ub.size());
 
-    /* disable console output */
-    model.set(GRB_IntParam_OutputFlag, 0);
+    /* change objective sense to maximize */
+    CATCH_SCIP( SCIPsetObjsense(scip, SCIP_OBJSENSE_MAXIMIZE) );
 
     /* create problem variables */
+    ostringstream namebuf;
     for( int j = 0; j < n; ++j )
     {
-      GRBVar xvar, yvar;
-      if (ub[j] < numeric_limits<double>::max())
-      {
-        xvar = model.addVar(lb[j], ub[j], 0.0, GRB_CONTINUOUS, "X");
-        yvar = model.addVar(lb[j], ub[j], 0.0, GRB_CONTINUOUS, "Y");
-      }
-      else
-      {
-        xvar = model.addVar(lb[j], GRB_INFINITY, 0.0, GRB_CONTINUOUS, "X");
-        yvar = model.addVar(lb[j], GRB_INFINITY, 0.0, GRB_CONTINUOUS, "Y");
-      }
-      xvars.push_back( xvar );
-      yvars.push_back( yvar );
+      SCIP_VAR *xvar, *yvar;
+
+      /* create the x SCIP_VAR object  and add to the scip problem */
+      namebuf.str("");
+      namebuf << "x#" << j;
+      retcode = SCIPcreateVar(scip, &xvar, namebuf.str().c_str(),
+                                   lb[j], SCIPinfinity(scip), 0.0,  // lb, ub, obj
+                                   SCIP_VARTYPE_CONTINUOUS, TRUE, FALSE, NULL, NULL, NULL, NULL, NULL);
+      retcode = SCIPaddVar(scip, xvar);
+      xvars.push_back(xvar);
+
+      /* create the y SCIP_VAR object  and add to the scip problem */
+      namebuf.str("");
+      namebuf << "y#" << j;
+      retcode = SCIPcreateVar(scip, &yvar, namebuf.str().c_str(),
+                                   lb[j], SCIPinfinity(scip), 0.0,  // lb, ub, obj
+                                   SCIP_VARTYPE_CONTINUOUS, TRUE, FALSE, NULL, NULL, NULL, NULL, NULL);
+      retcode = SCIPaddVar(scip, yvar);
+      yvars.push_back(yvar);
+
     }
 
     /* create constraints Ax<=b */
     for( int i = 0; i < m; ++i )
     {
-      GRBLinExpr expr = 0;
-      for( int j = 0; j < n; ++j )
-        expr += A[i][j]*xvars[j];
-      string name = "CX_" + itos(i);
-      model.addConstr(expr, GRB_LESS_EQUAL, b[i], name);
+       SCIP_CONS* cons;
+       namebuf.str("");
+       namebuf<<"CX_"<<i;
+
+       /* create SCIP_CONS object */
+       retcode = SCIPcreateConsBasicLinear(scip, & cons, namebuf.str().c_str(),
+                                                0, NULL, NULL, -SCIPinfinity(scip), b[i]); // nvars, vars, vals, lhs, rhs
+
+       /* add the vars belonging to field in this row to the constraint */
+       for( int j = 0; j < n; ++j )
+          retcode = SCIPaddCoefLinear(scip, cons, xvars[j], A[i][j]);
+
+       /* add the constraint to scip */
+       retcode = SCIPaddCons(scip, cons);
+
+       conss.push_back(cons);
     }
 
     /* create constraints Ay<=b */
     for( int i = 0; i < m; ++i )
     {
-      GRBLinExpr expr = 0;
-      for( int j = 0; j < n; ++j )
-        expr += A[i][j]*yvars[j];
-      string name = "CY_" + itos(i);
-      model.addConstr(expr, GRB_LESS_EQUAL, b[i], name);
+       SCIP_CONS * cons;
+       namebuf.str("");
+       namebuf<<"CY_"<<i;
+
+       /* create SCIP_CONS object */
+       retcode = SCIPcreateConsBasicLinear(scip, & cons, namebuf.str().c_str(),
+                                                0, NULL, NULL, -SCIPinfinity(scip), b[i]); // nvars, vars, vals, lhs, rhs
+
+       /* add the vars belonging to field in this row to the constraint */
+       for( int j = 0; j < n; ++j )
+          retcode = SCIPaddCoefLinear(scip, cons, yvars[j], A[i][j]);
+
+       /* add the constraint to scip */
+       retcode = SCIPaddCons(scip, cons);
+
+       conss.push_back(cons);
     }
 
   }
@@ -76,6 +110,24 @@ public:
   Polytope(const matrix &A, const vector<double> &b,
            const vector<double> &lb, const vector<double> &ub)
   {
+    /* initialize scip */
+    CATCH_SCIP( SCIPcreate(&scip) );
+
+    /* load default plugins */
+    CATCH_SCIP( SCIPincludeDefaultPlugins(scip) );
+
+    /* load solving settings */
+    CATCH_SCIP( SCIPreadParams(scip, "settingsfile.set") );
+
+    /* disable scip output to stdout */
+    SCIPmessagehdlrSetQuiet(SCIPgetMessagehdlr(scip), TRUE);
+
+    /* disable heuristics */
+    CATCH_SCIP( SCIPsetHeuristics(scip, SCIP_PARAMSETTING_OFF, TRUE) );
+
+    /* create an empty problem */
+    CATCH_SCIP( SCIPcreateProb(scip, "width", NULL, NULL, NULL, NULL, NULL, NULL, NULL) );
+
     /* add basic variables and constraints */
     initialize(A, b, lb, ub);
 
@@ -92,49 +144,94 @@ public:
 
   ~Polytope()
   {
-    xvars.clear();
-    yvars.clear();
+    try
+    {
+      /* release variables */
+       for(int i = 0; i < xvars.size(); ++i)
+         CATCH_SCIP( SCIPreleaseVar(scip, &xvars[i]) );
+       for(int i = 0; i < yvars.size(); ++i)
+         CATCH_SCIP( SCIPreleaseVar(scip, &yvars[i]) );
+       xvars.clear();
+       yvars.clear();
+
+       /* release constraints */
+       for(int i = 0; i < conss.size(); ++i)
+         CATCH_SCIP( SCIPreleaseCons(scip, &conss[i]) );
+       conss.clear();
+
+       /* free the scip problem */
+       CATCH_SCIP( SCIPfree(&scip) );
+    }
+    catch ( const char* msg )
+    {
+       cerr << msg << endl;
+    }
   }
 
   double distance(vector<double> w, int k, double* alpha, double* beta)
   {
     int n = xvars.size();
-    assert(w.size() == n);
 
     /* change objective function */
-    GRBLinExpr obj = 0;
     for( int j = 0; j < n; ++j )
-      obj+=w[j]*xvars[j] - w[j]*yvars[j];
-    model.setObjective(obj, GRB_MAXIMIZE);
+    {
+      CATCH_SCIP( SCIPchgVarObj(scip, xvars[j], w[j]) );
+      CATCH_SCIP( SCIPchgVarObj(scip, yvars[j], -w[j]) );
+    }
 
     /* add new rows */
-    vector<GRBConstr> added_conss;
+    ostringstream namebuf;
+    vector<SCIP_CONS*> added_conss;
     for( int i = 1; i < k; ++i )
     {
-       GRBLinExpr expr;
+       SCIP_CONS* cons;
+       namebuf.str("");
+       namebuf<<"b_"<<i;
+
+       /* create SCIP_CONS object */
+       CATCH_SCIP( SCIPcreateConsBasicLinear(scip, & cons, namebuf.str().c_str(),
+                                                0, NULL, NULL, 0.0, 0.0) ); // nvars, vars, vals, lhs, rhs
+
+       /* add the vars belonging to field in this row to the constraint */
        for( int j = 0; j < n; ++j )
-         expr += basis[i-1][j]*xvars[j] - basis[i-1][j]*yvars[j];
-       string name = "N_" + itos(i);
-       GRBConstr cons = model.addConstr(expr, GRB_EQUAL, 0.0, name);
+       {
+         CATCH_SCIP( SCIPaddCoefLinear(scip, cons, xvars[j], basis[i-1][j]) );
+         CATCH_SCIP( SCIPaddCoefLinear(scip, cons, yvars[j], -basis[i-1][j]) );
+       }
+
+       /* add the constraint to scip */
+       CATCH_SCIP( SCIPaddCons(scip, cons) );
+
        added_conss.push_back(cons);
     }
 
     /* solve */
-    model.optimize();
+    CATCH_SCIP( SCIPsolve(scip) );
 
     /* get solution */
-    double bestsol = model.get(GRB_DoubleAttr_ObjVal);
+    SCIP_SOL *sol = SCIPgetBestSol(scip);
+    double bestsol = SCIPgetSolOrigObj(scip, sol);
 
     /* get dual variables for last two constraints */
-    for(int i = 0; i < added_conss.size(); ++i)
+    SCIP_Bool success = FALSE;
     if ( k > 1 && alpha != nullptr)
-      *alpha = -added_conss[k-2].get(GRB_DoubleAttr_Pi);
+    {
+      SCIPconsGetDualsol(scip, added_conss[k-2], alpha, &success);
+      assert(success);
+    }
     if ( k > 2 && beta != nullptr)
-      *beta = -added_conss[k-3].get(GRB_DoubleAttr_Pi);
+    {
+      SCIPconsGetDualsol(scip, added_conss[k-3], beta, &success);
+      assert(success);
+    }
 
     /* release transformed problem and added constraints */
+    CATCH_SCIP( SCIPfreeTransform(scip) );
     for(int i = 0; i < added_conss.size(); ++i)
-      model.remove(added_conss[i]);
+    {
+      CATCH_SCIP( SCIPdelCons(scip, added_conss[i]) );
+      CATCH_SCIP( SCIPreleaseCons(scip, &added_conss[i]) );
+    }
     added_conss.clear();
 
     return bestsol;
@@ -148,38 +245,77 @@ public:
     int n = xvars.size();
 
     /* change objective function */
-    GRBLinExpr obj;
     for( int j = 0; j < n; ++j )
-      obj += basis[p-1][j]*xvars[j] - basis[p-1][j]*yvars[j];
-    model.setObjective(obj, GRB_MAXIMIZE);
+    {
+      CATCH_SCIP( SCIPchgVarObj(scip, xvars[j], basis[p-1][j]) );
+      CATCH_SCIP( SCIPchgVarObj(scip, yvars[j], -basis[p-1][j]) );
+    }
 
     /* add new rows */
-    vector<GRBConstr> added_conss;
+    ostringstream namebuf;
+    vector<SCIP_CONS*> added_conss;
     for( int i = 1; i < k; ++i )
     {
-       GRBLinExpr expr;
+       SCIP_CONS* cons;
+       namebuf.str("");
+       namebuf<<"b_"<<i;
+
+       /* create SCIP_CONS object */
+       CATCH_SCIP( SCIPcreateConsLinear(scip, &cons, namebuf.str().c_str(),
+                                        0, NULL, NULL, 0.0, 1.0, // nvars, vars, vals, lhs, rhs
+ 					                              TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE) );
+       /* add the vars belonging to field in this row to the constraint */
        for( int j = 0; j < n; ++j )
-         expr += basis[i-1][j]*xvars[j] -basis[i-1][j]*yvars[j];
-       string name = "N_" + itos(i);
-       GRBConstr cons = model.addConstr(expr, GRB_EQUAL, 0.0, name);
+       {
+         CATCH_SCIP( SCIPaddCoefLinear(scip, cons, xvars[j], basis[i-1][j]) );
+         CATCH_SCIP( SCIPaddCoefLinear(scip, cons, yvars[j], -basis[i-1][j]) );
+       }
+
+       /* add the constraint to scip */
+       CATCH_SCIP( SCIPaddCons(scip, cons) );
+
        added_conss.push_back(cons);
     }
 
     /* solve */
-    model.optimize();
+    CATCH_SCIP( SCIPsolve(scip) );
 
     /* get solution */
-    double bestsol = model.get(GRB_DoubleAttr_ObjVal);
+    SCIP_SOL *sol = SCIPgetBestSol(scip);
+    double bestsol = SCIPgetSolOrigObj(scip, sol);
 
     /* get dual variables for last two constraints */
+    SCIP_Bool success = FALSE;
+    double sss;
+    cout << "nrows " << SCIPgetNLPRows(scip) <<endl;
+
+    for(int i = 0; i < added_conss.size(); ++i)
+    {
+
+      SCIP_ROW* row = SCIPconsGetRow(scip, added_conss[i]);
+      //assert(row!=nullptr);
+      //sss = SCIProwGetDualsol(row);
+      SCIPconsGetDualsol(scip, added_conss[i], &sss, &success);
+      cout << sss <<endl;
+    }
     if ( k > 1 && alpha != nullptr)
-      *alpha = -added_conss[k-2].get(GRB_DoubleAttr_Pi);
+    {
+      SCIPconsGetDualsol(scip, added_conss[k-2], alpha, &success);
+      assert(success);
+    }
     if ( k > 2 && beta != nullptr)
-      *beta = -added_conss[k-3].get(GRB_DoubleAttr_Pi);
+    {
+      SCIPconsGetDualsol(scip, added_conss[k-3], beta, &success);
+      assert(success);
+    }
 
     /* release transformed problem and added constraints */
+    CATCH_SCIP( SCIPfreeTransform(scip) );
     for(int i = 0; i < added_conss.size(); ++i)
-      model.remove(added_conss[i]);
+    {
+      CATCH_SCIP( SCIPdelCons(scip, added_conss[i]) );
+      CATCH_SCIP( SCIPreleaseCons(scip, &added_conss[i]) );
+    }
     added_conss.clear();
 
     return bestsol;
@@ -196,6 +332,7 @@ main(
      char ** argv
      )
 {
+  SCIP_RETCODE retcode;
 
   /* get problem data */
   int n = 2;
@@ -206,11 +343,10 @@ main(
   vector<double> lb(n);
   vector<double> ub(n);
 
-
   A = { {-1, -7}, {2, 7}, {-5, 4} };
   b = { -7, 14, 4 };
   lb = {0,0};
-  ub = {numeric_limits<double>::max(), numeric_limits<double>::max()};
+  ub = {1000, 1000};
 
   /* initialize polytope */
   Polytope P(A, b, lb, ub);
@@ -223,7 +359,7 @@ main(
   int i = 1;
   while (i < n)
   {
-    cout << "i: " << i << endl;
+    cout << i << endl;
     /* get F[i] */
     if ( F[i] < 0 )
       F[i] = P.distance(i, i, nullptr, nullptr);
@@ -255,8 +391,8 @@ main(
         vec1[j] = P.basis[i][j] + ceil(alpha)*P.basis[i-1][j];
         vec2[j] = P.basis[i][j] + floor(alpha)*P.basis[i-1][j];
       }
-      double h1 = P.distance(vec1, i, nullptr, &beta1);
-      double h2 = P.distance(vec2, i, nullptr, &beta2);
+      double h1 = P.distance(vec1, i+1, &alpha, &beta1);
+      double h2 = P.distance(vec2, i+1, &alpha, &beta2);
       if (h1<h2)
       {
         mu = ceil(alpha); fpp = h1; beta = beta1;
@@ -267,8 +403,6 @@ main(
       }
 
     }
-
-    cout << "mu= " << mu << endl;
 
     /* update b^{i+1} */
     for( int j = 0; j < n; ++j )
@@ -301,15 +435,6 @@ main(
       cout << "forward" << endl;
       F[i+1] = h;
       i++;
-    }
-
-    for( int i = 0; i < n; ++i )
-    {
-      for( int j = 0; j < n; ++j )
-      {
-        cout << P.basis[i][j] << " ";
-      }
-      cout << endl;
     }
 
   }
